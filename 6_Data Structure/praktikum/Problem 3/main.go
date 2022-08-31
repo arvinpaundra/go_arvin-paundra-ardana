fe5f8df5e5a53137a267d2edd05598bfcdd0fa1a
arvinpaundra <arvinpaundra06@gmail.com>
2022-08-31 23:50:04 +0700

    Add file that solve Problem 3

---
+++ 6_Data Structure/praktikum/Problem 3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func pairSum(arr []int, target int) []int {
+	var arrTemp []int
+
+	for i := range arr {
+		for j := 0; j < len(arr); j++ {
+			if arr[i]+arr[j] == target {
+				arrTemp = append(arrTemp, i, j)
+				break
+			}
+		}
+
+		if len(arrTemp) > 0 {
+			break
+		}
+	}
+
+	return arrTemp
+}
+
+func main() {
+	fmt.Println(pairSum([]int{1, 2, 3, 4, 6}, 6))
+	fmt.Println(pairSum([]int{2, 5, 9, 11}, 11))
+	fmt.Println(pairSum([]int{1, 3, 5, 7}, 12))
+	fmt.Println(pairSum([]int{1, 4, 6, 8}, 10))
+	fmt.Println(pairSum([]int{1, 5, 6, 7}, 6))
+}
