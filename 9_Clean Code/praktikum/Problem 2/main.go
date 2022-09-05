7e98d5f97697d52a8bc67fdf212babf9ae243f0d
arvinpaundra <arvinpaundra06@gmail.com>
2022-09-05 14:54:32 +0700

    Add file that solve Problem 2

---
+++ 9_Clean Code/praktikum/Problem 2/main.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+/*
+	BEFORE
+*/
+
+// type kendaraan struct {
+// 	totalroda       int
+// 	kecepatanperjam int
+// }
+
+// type mobil struct {
+// 	kendaraan
+// }
+
+// func (m *mobil) berjalan() {
+// 	m.tambahkecepatan(10)
+// }
+
+// func (m *mobil) tambahkecepatan(kecepatanbaru int) {
+// 	m.kecepatanperjam = m.kecepatanperjam + kecepatanbaru
+// }
+
+// func main() {
+// 	mobilcepat := mobil{}
+// 	mobilcepat.berjalan()
+// 	mobilcepat.berjalan()
+// 	mobilcepat.berjalan()
+// 	fmt.Println(mobilcepat)
+
+// 	mobillamban := mobil{}
+// 	mobillamban.berjalan()
+// 	fmt.Println(mobillamban)
+// }
+
+/*
+	AFTER
+*/
+
+type Kendaraan struct {
+	TotalRoda       int
+	KecepatanPerJam int
+}
+
+type Mobil struct {
+	Kendaraan
+}
+
+func (m *Mobil) Berjalan() {
+	m.TambahKecepatan(10)
+}
+
+func (m *Mobil) TambahKecepatan(kecepatanBaru int) {
+	m.KecepatanPerJam = m.KecepatanPerJam + kecepatanBaru
+}
+
+func main() {
+	mobilCepat := Mobil{}
+	mobilCepat.Berjalan()
+	mobilCepat.Berjalan()
+	mobilCepat.Berjalan()
+	fmt.Println(mobilCepat)
+
+	mobilLamban := Mobil{}
+	mobilLamban.Berjalan()
+	fmt.Println(mobilLamban)
+}
