53e7c35a6f5a734e73fd7f559f8dd40aa466e556
arvinpaundra <arvinpaundra06@gmail.com>
2022-09-05 14:54:22 +0700

    Add file that solve Problem 1

---
+++ 9_Clean Code/praktikum/Problem 1/main.go
@@ -0,0 +1,57 @@
+package main
+
+/*
+	BEFORE
+*/
+
+// type user struct {
+// 	id       int
+// 	username int
+// 	password int
+// }
+
+// type userservice struct {
+// 	t []user
+// }
+
+// func (u userservice) getallusers() []user {
+// 	return u.t
+// }
+
+// func (u userservice) getuserbyid(id int) user {
+// 	for _, r := range u.t {
+// 		if id == r.id {
+// 			return r
+// 		}
+// 	}
+
+// 	return user{}
+// }
+
+/*
+	AFTER
+*/
+
+type User struct {
+	ID       int
+	Username int
+	Password int
+}
+
+type UserService struct {
+	Users []User
+}
+
+func (u UserService) GetAllUsers() []User {
+	return u.Users
+}
+
+func (u UserService) GetUserByID(id int) User {
+	for _, user := range u.Users {
+		if id == user.ID {
+			return user
+		}
+	}
+
+	return User{}
+}
